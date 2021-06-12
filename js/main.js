e4f7894be7ad32dfa12d70740f9f4918cb395523
Amukozoberit <mwasheb@gmail.com>
2021-06-12 09:50:54 +0300

    set max and value of date to currentday(today) changes with days

---
+++ js/main.js
@@ -0,0 +1,19 @@
+//  set the maximum date to today to avoid user from inputing a birthdate not today
+let today = new Date();
+let dd = today.getDate();
+let mm = today.getMonth() + 1; //January is 0!
+let yyyy = today.getFullYear();
+if (dd < 10) {
+    dd = '0' + dd //adds a 0 before for rep
+}
+if (mm < 10) {
+    mm = '0' + mm //adds a 0 before for rep
+}
+console.log(yyyy);
+today = yyyy + '-' + mm + '-' + dd;
+document.getElementById('day').setAttribute('max', today);
+document.getElementById('day').setAttribute('value', today);
+
+function validateDate() {
+
+}
